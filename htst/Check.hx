9a130cf1df071479fb9b29aae4103d1ceb3041c0
Matt Chudleigh <matt.chudleigh@gmail.com>
2020-05-30 08:50:54 -0700

    Refactor unit test extra checks into own file

---
+++ htst/Check.hx
@@ -0,0 +1,55 @@
+package htst;
+
+import utest.Assert;
+
+class Check {
+	public static function point(x, y, z, p, ?pos:haxe.PosInfos) {
+		Assert.floatEquals(x, p.x, 'X component wrong. Expected: $x Got: ${p.x}', pos);
+		Assert.floatEquals(y, p.y, 'Y component wrong. Expected: $y Got: ${p.x}', pos);
+		Assert.floatEquals(z, p.z, 'Z component wrong. Expected: $z Got: ${p.z}', pos);
+	}
+	public static function floatArray(exp:Array<Float>, val:Array<Float>, ?pos:haxe.PosInfos) {
+		Assert.equals(exp.length, val.length,
+			 'Array length wrong. Expected: ${exp.length} Got: ${val.length}', pos);
+		var minLen = Std.int(Math.min(exp.length, val.length));
+		for(i in 0...minLen) {
+			Assert.floatEquals(exp[i], val[i],
+				'Float array element mismatch at $i. Expected: ${exp[i]} Got: ${val[i]}', pos);
+		}
+	}
+	public static function array(exp:Array<Dynamic>, val:Array<Dynamic>, ?pos:haxe.PosInfos) {
+		Assert.equals(exp.length, val.length,
+			'Array length wrong. Expected: ${exp.length} Got: ${val.length}', pos);
+		var minLen = Std.int(Math.min(exp.length, val.length));
+		for(i in 0...minLen) {
+			Assert.equals(exp[i], val[i],
+				'Array element mismatch at $i. Expected: ${exp[i]} Got: ${val[i]}', pos);
+		}
+	}
+
+	public static function arrayOptions(exp:Array<Dynamic>, val:Array<Dynamic>, ?pos:haxe.PosInfos) {
+		// Scratch
+		var arrayClass = Type.resolveClass("Array");
+
+		Assert.equals(exp.length, val.length,
+			'Array length wrong. Expected: ${exp.length} Got: ${val.length}', pos);
+		var minLen = Std.int(Math.min(exp.length, val.length));
+		for(i in 0...minLen) {
+			var expType = Type.getClass(exp[i]);
+			if (expType == arrayClass) {
+				// This is a list of possible options. Check that one matches
+				var optionList = cast (exp[i], Array<Dynamic>);
+				var found = false;
+				for (o in optionList) {
+					if (val[i] == o) found = true;
+				}
+				Assert.isTrue(found,
+					'Option failure in array at element $i', pos);
+			} else {
+			Assert.equals(exp[i], val[i],
+				'Array element mismatch at $i. Expected: ${exp[i]} Got: ${val[i]}', pos);
+			}
+		}
+	}
+
+}
